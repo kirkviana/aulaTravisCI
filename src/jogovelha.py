7a1dcac1751b369ae92a3cbb2a8a020891f480f2
kirkviana <47369894+kirkviana@users.noreply.github.com>
2019-04-01 21:18:56 -0300

    Update jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,16 +1,15 @@
-jogo = jogovelha.inicializar()
+def inicializar():
+  tab = []
+  for i in range(3):
+    linha = []
+    for j in range(3):
+      linha.append(".")
+    tab.append(linha)
+  return tab
 
-if len(jogo) != 3:
-  erroInicializar = True
-else:
-  for linha in jogo:
-    if len(linha) != 3:
-      erroInicializar = True
-    else:
-      for elemento in linha:
-        if elemento != '.':
-          erroInicializar = True
-if erroInicializar:
-  sys.exit(1)
-else:
-  sys.exit(0)
+def main():
+  jogo = inicializar()
+  print(jogo)
+
+if __name__ == "__main__":
+  main()
